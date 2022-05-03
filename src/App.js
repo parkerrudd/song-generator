eab8383df11f6358bdeea8083bee4d25488d7c71
Parker Rudd <parkerbrudd@gmail.com>
2022-05-03 13:25:55 -0600

    removed window.hash from dependency and added a test console.log

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ function App() {
   useEffect(() => {
     if (window.location.hash) {
       const { access_token, expires_in, token_type } = getReturnedParamsFromSpotify(window.location.hash);
-
+      console.log('hello'); 
       localStorage.clear();
       localStorage.setItem("accessToken", access_token);
       localStorage.setItem("tokenType", token_type);
       localStorage.setItem("expiresIn", expires_in);
     }
-  }, [window.location]);
+  }, []);
 
   useEffect(() => {
     if (window.location.href.indexOf('access_token') > -1) {
